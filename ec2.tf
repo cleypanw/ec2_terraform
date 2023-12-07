9d2141ae7790cfa261f315cd6eccec3550fce34b
chrisley75 <cley@paloaltonetworks.com>
2023-12-07 11:53:16 +0100

    add terraform outputs

--- ec2.tf
+++ ec2.tf
@@ -5,22 +5,22 @@ resource "aws_key_pair" "ssh_key" {
 
 resource "aws_instance" "ec2instance" {
 
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.medium"
-  subnet_id              = module.vpc.public_subnets[0]
-  private_ip             = "10.0.4.10"
-  vpc_security_group_ids = ["${aws_security_group.ec2-instance.id}"]
+  ami                       = data.aws_ami.ubuntu.id
+  instance_type             = "t3.medium"
+  subnet_id                 = module.vpc.public_subnets[0]
+  private_ip                = "10.0.4.10"
+  vpc_security_group_ids    = ["${aws_security_group.ec2-instance.id}"]
 
   #checkov:skip=CKV_AWS_88
-  associate_public_ip_address = true
+  associate_public_ip_address   = true
 
   key_name = "${aws_key_pair.ssh_key.key_name}"
 
   root_block_device {
-    delete_on_termination = true
-    volume_size           = 20
-    volume_type           = "gp2"
-    encrypted             = true
+    delete_on_termination   = true
+    volume_size             = 20
+    volume_type             = "gp2"
+    encrypted               = true
   }
 
   ## https://docs.bridgecrew.io/docs/bc_aws_general_31
