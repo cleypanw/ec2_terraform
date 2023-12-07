64ec988e34a99086e21d8cfbf800702aae461a09
chrisley75 <cley@paloaltonetworks.com>
2023-12-07 11:45:28 +0100

    first commit

--- variables.tf
+++ variables.tf
@@ -9,16 +9,6 @@ variable "instance_type" {
         default = "t2.micro"
 }
 
-variable "subnet_id" {
-        description = "The VPC subnet the instance(s) will be created in"
-        default = "subnet-07ebbe60"
-}
-
-variable "ami_id" {
-        description = "The AMI to use"
-        default = "ami-09d56f8956ab235b3"
-}
-
 variable "number_of_instances" {
         description = "number of instances to be created"
         default = 1
